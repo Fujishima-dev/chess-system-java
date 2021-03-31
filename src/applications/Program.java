cdc3588d8caca1cdcba35c1955faf569127cd511
Fujishima-dev <fujishima.dev@gmail.com>
2021-03-31 09:59:32 -0300

    First class: Position

--- src/applications/Program.java
+++ src/applications/Program.java
@@ -1,6 +1,12 @@
 package applications;
 
-public class Program {
+import boardgame.Position;
 
+public class Program {
+     public static void main(String[] args) {
 	
+	Position pos = new Position(3,5);
+    System.out.println(pos);
+	
+ }
 }
